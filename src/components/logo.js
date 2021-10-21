e0aee25d580b7077709e9e832786644ea520a024
Jara Santamaria Martinez <jarasmar@gmail.com>
2021-10-21 12:08:59 +0100

    Add link to logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Logo() {
     return (
-        <div className="logo">Logo</div>
+        <div className="logo"><Link to='/'>ECO</Link></div>
     )
 }
 
